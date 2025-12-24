19fc6fb81142144c84e08c3d18190403bed97719
Your Name <your_email@example.com>
2025-12-24 20:50:30 +0530

    Hero Section UI

--- src/part/Home.jsx
+++ src/part/Home.jsx
@@ -1,13 +1,31 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams , useNavigate } from 'react-router-dom'
 import HomeStyle from './css/Home.module.css'
 
 export default function Home() {
   const { name } = useParams()
+
+  const navigate = useNavigate()
+
+  const Logout = () =>{
+    navigate('/')
+  }
+
   return (
     <>
       <div className={HomeStyle.HomeSection}>
-          <h1 className={HomeStyle.WelcomeText}>Welcome To Home  {name} .. !</h1>
+        <nav className={HomeStyle.Nav}>
+          <h1 className={HomeStyle.navTitle}>Repofy</h1>
+          <button className={HomeStyle.Logout} onClick={Logout}>LogOut</button>
+        </nav>
+
+        <div className={HomeStyle.HomeContainer}>
+          <ul className={HomeStyle.WelcomeText}>
+            <li>Welcome , </li>
+            <li>{name}</li>
+          </ul>
+        </div>
+
       </div>
     </>
   )
