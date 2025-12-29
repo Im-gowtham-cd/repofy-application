384ef067f2e60ee4ec8d8a868ef76f8855cc1e3f
Your Name <your_email@example.com>
2025-12-29 09:38:35 +0530

    UI Bug  Fix

--- src/part/Home.jsx
+++ src/part/Home.jsx
@@ -20,7 +20,9 @@ export default function Home() {
             <div className={HomeStyle.createroomcontainer}>
               <h1 className={HomeStyle.RoomTitle}>Create Room</h1>
               <div className={HomeStyle.RoomContent}>
-
+                  <form action="">
+                    <span>Name</span>
+                  </form>
               </div>
               <ul className={HomeStyle.RoomSubmit}>
                 <button className={HomeStyle.CloseButton} onClick={() => setCreateRoom(false)}>Create</button>
