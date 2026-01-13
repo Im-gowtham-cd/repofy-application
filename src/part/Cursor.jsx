628a8bd049173ff37911d59a063838c69aa18bb0
Im-gowtham-cd <gowthamcdstudies@gmail.com>
2026-01-13 14:02:30 +0530

    Cursor UI Chnage

--- src/part/Cursor.jsx
+++ src/part/Cursor.jsx
@@ -5,10 +5,12 @@ export default function Cursor() {
 
     useEffect(() => {
         const cursor = document.getElementById("cursor");
+        const repofy = document.getElementById("Repofy");
         const mouse = e => {
             let y = e.clientY - cursor.offsetHeight / 2;
             let x = e.clientX - cursor.offsetWidth / 2;
             cursor.style.transform = `translate(${x}px , ${y}px)`;
+            repofy.style.transform = `rotate(${y}Deg)`;
         }
 
         window.addEventListener('mousemove', mouse);
@@ -22,12 +24,16 @@ export default function Cursor() {
     return (
         <>
             <div className={CursorStyle.cursor} id="cursor">
-                <ul className={CursorStyle.eyes}>
+{/*             <ul className={CursorStyle.eyes}>
                     <li></li>
                     <li></li>
                 </ul>
                 <span className={CursorStyle.mouth}></span>
-                <span className={CursorStyle.mouthCircle}></span>
+                <span className={CursorStyle.mouthCircle}></span> */}
+
+                <div id="Repofy" className={CursorStyle.sparks}>
+                    <span>Repofy</span>
+                </div>
             </div>
         </>
     )
